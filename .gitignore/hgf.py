c353b84bd435b0213cc3a87f446caaa2cd591963
mohanramofficial <36396379+mohanramofficial@users.noreply.github.com>
2018-03-14 19:39:14 +0530

    Update and rename .gitignore to hgf.py

---
+++ .gitignore/hgf.py
@@ -0,0 +1,12 @@
+https://github.com/mohanramofficial/hgfhgf/blob/master/.gitignore/vhgvh.py
+''' Python program to find the
+multiplication table (from 1 to 10)'''
+
+num = 12
+
+# To take input from the user
+# num = int(input("Display multiplication table of? "))
+
+# use for loop to iterate 10 times
+for i in range(1, 11):
+   print(num,'x',i,'=',num*i)
